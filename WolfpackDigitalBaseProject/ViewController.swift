c9bd53d328f31f38c484487288474395c566109d
Bianca Felecan <bianca@wolfpack-digital.com>
2019-12-01 19:07:01 +0200

    Initial Commit

---
+++ WolfpackDigitalBaseProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WolfpackDigitalBaseProject
+//
+//  Created by Bianca Felecan on 01/12/2019.
+//  Copyright © 2019 Wolfpack Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
