f3dae2213943c46f246d437c9076f2ec89e8a36c
Bianca Felecan <bianca@wolfpack-digital.com>
2019-12-02 15:22:03 +0200

    first commit

---
+++ WolfpackDigitalBaseProject/BaseProtocols/Bindable.swift
@@ -0,0 +1,13 @@
+//
+//  Bindable.swift
+//  WolfpackDigitalBaseProject
+//
+//  Created by Bianca Felecan on 02/12/2019.
+//  Copyright © 2019 Wolfpack Digital. All rights reserved.
+//
+
+import UIKit
+
+class Bindable: NSObject {
+
+}
